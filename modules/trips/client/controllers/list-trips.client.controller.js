ec649cb75b428d17930e17d307fef00db7538a5d
Matt Petters <matt@petters.io>
2016-09-15 01:42:29 -0400

    Client side user based filtering on trips working, in real implementation would need to protect endpoints

--- modules/trips/client/controllers/list-trips.client.controller.js
+++ modules/trips/client/controllers/list-trips.client.controller.js
@@ -5,33 +5,36 @@
     .module('trips')
     .controller('TripsListController', TripsListController);
 
-  TripsListController.$inject = ['TripsService', 'Authentication'];
+  TripsListController.$inject = ['TripsService', 'Authentication', '$filter'];
 
-  function TripsListController(TripsService, Authentication) {
+  function TripsListController(TripsService, Authentication, $filter) {
     var vm = this;
     vm.authentication = Authentication;
-    var trips;
     //http://blog.ninja-squad.com/2015/05/28/angularjs-promises/
-    TripsService.query().then(function(response) {
-      var trips = response.data;
+    //filtering from resource - http://stackoverflow.com/questions/28906953/filters-not-working-on-array-from-resource
+    //creating filters correctly - http://stackoverflow.com/questions/20292638/angular-filter-exactly-on-object-key
+    TripsService.query().$promise.then(function(response) {
+      // console.log(vm.authentication.user._id + " is the auth user id");
+      //return only the trips which have the current user as a passenger
+      // for (var trip in trips) {
+      //   console.log(trip.name + " is the trip");
+      //   for (var passenger in trip.passengers) {
+      //     console.log(passenger.displayName + " is the passenger");
+      //     console.log(passenger._id + " is the passenger ID");
+      //     if (vm.authentication.user._id !== passenger._id) {
+      //       console.log(vm.authentication.user._id !== passenger._id);
+      //       var index = vm.trips.indexOf(trip);
+      //       if (index > -1) {
+      //         console.log("Got to the splicing part");
+      //         trips.splice(index, 1);
+      //       }
+      //     }
+      //   }
+      // }
+      vm.trips = response;
+      vm.userId = vm.authentication.user._id;
     });
 
-      console.log(vm.authentication.user._id + " is the auth user id");
-      //return only the trips which have the current user as a passenger
-      for (var trip in trips) {
-        console.log(trip + " is the trip");
-        for (var passenger in trip.passengers) {
-          console.log(passenger + " is the passenger");
-          console.log(passenger._id + " is the passenger ID");
-          if (vm.authentication.user._id !== passenger._id) {
-            var index = vm.trips.indexOf(trip);
-            if (index > -1) {
-              console.log("Got to the splicing part");
-              trips.splice(index, 1);
-            }
-          }
-        }
-      }
-      vm.trips = trips;
+
 }
 })();
